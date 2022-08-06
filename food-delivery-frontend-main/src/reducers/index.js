5dddf2a47377d3577f1ffac87126f7b39a12beea
bharathkrishnasivadas <102355043+bharathkrishnasivadas@users.noreply.github.com>
2022-08-06 12:29:59 +0530

    Add files via upload

---
+++ food-delivery-frontend-main/src/reducers/index.js
@@ -0,0 +1,18 @@
+import { combineReducers } from "redux";
+import cartReducer from "./cartReducer";
+import pizzaReducer from "./pizzaReducers";
+import {signInReducer,signUpReducer} from './authReducer'
+import { addressReducer } from "./addressReducer";
+import { orderReducer,orderDetail, searchItems } from "./odersReducer";
+import { wishlistReducer } from "./wishlistReducer";
+export default combineReducers({
+    allPizza:pizzaReducer,
+    cart:cartReducer,
+    user:signInReducer,
+    userRegister:signUpReducer,
+    address:addressReducer,
+    order:orderReducer,
+    orderDetails:orderDetail,
+    search:searchItems,
+    wishlist:wishlistReducer
+})
